51a2a391868b0d87da8dd0ce3a49c232d2a102ba
being24 <34680324+being24@users.noreply.github.com>
2021-08-01 20:47:25 +0900

    ⚡️ 通知無しで追加可能に変更

--- cogs/thread_keeper.py
+++ cogs/thread_keeper.py
@@ -69,7 +69,8 @@ class Hofumi(commands.Cog, name='Thread管理用cog'):
             if role is not None:
                 content = f'{content}{role.mention}'
 
-        await thread.send(f"{content} スレッドが作成されました")
+        msg = await thread.send("スレッドが作成されました")
+        await msg.edit(content=f'{content} {msg.content}')
 
     def return_estimated_archive_time(
             self, thread: discord.Thread) -> datetime:
@@ -196,15 +197,16 @@ class Hofumi(commands.Cog, name='Thread管理用cog'):
         await self.call_of_thread(thread)
 
         # 低速モードを引き継ぎ
-        try:
-            if thread.parent.slowmode_delay == 0:
-                return
-            await thread.edit(slowmode_delay=thread.parent.slowmode_delay)
-            msg = await thread.send("低速モードを設定しました")
-            await self.c.autodel_msg(msg)
-        except discord.Forbidden:
-            print("権限不足")
-            print(thread)
+        if thread.parent is not None:
+            try:
+                if thread.parent.slowmode_delay == 0:
+                    return
+                await thread.edit(slowmode_delay=thread.parent.slowmode_delay)
+                msg = await thread.send("低速モードを設定しました")
+                await self.c.autodel_msg(msg)
+            except discord.Forbidden:
+                print("権限不足")
+                print(thread)
 
         # DBの設定を確認、管理対象としてDBに入れる
         if await self.guild_setting_mng.is_full_maintainance(thread.guild.id):
