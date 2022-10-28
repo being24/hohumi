c37281185804a0866d62094446d9f4cdea0e1d4c
being <34680324+being24@users.noreply.github.com>
2022-10-28 21:03:24 +0900

    💚

--- update.sh
+++ update.sh
@@ -1,5 +1,3 @@
-docker stop hohumi
-docker rm hohumi
-# docker pull ghcr.io/being24/hohumi:latest
-# docker run -d -v hohumi-data:/opt/hohumi/data -v hohumi-log:/opt/hohumi/log --env-file .env --restart=always --name=hohumi ghcr.io/being24/hohumi
+docker compose down
+docker compose pull
 docker compose up -d
